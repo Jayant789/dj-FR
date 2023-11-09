bdb753a5d626653a619ca90d8abb1237ade42d7d
Jayant <jayantlaldas@gmail.com>
2023-11-09 17:07:20 +0530

    i frame

--- faceRecog/api/views.py
+++ faceRecog/api/views.py
@@ -1,9 +1,16 @@
-from django.http import StreamingHttpResponse
 from .camera import VideoCamera
+from django.http import StreamingHttpResponse
+from django.views.decorators.clickjacking import xframe_options_exempt
+from django.http import HttpResponse
 
 
+@xframe_options_exempt
 def video_feed(request):
-    return StreamingHttpResponse(
+    response = StreamingHttpResponse(
         VideoCamera().generate_frames(),
         content_type="multipart/x-mixed-replace;boundary=frame",
     )
+
+    # Set CORS headers explicitly
+
+    return response
