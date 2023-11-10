cfd7215c8113b3b91011e8e5768bb16b92873e6e
Jayant <jayantlaldas@gmail.com>
2023-11-10 12:55:45 +0530

    cameras option

--- faceRecog/api/views.py
+++ faceRecog/api/views.py
@@ -6,8 +6,14 @@ from django.http import HttpResponse
 
 @xframe_options_exempt
 def video_feed(request):
+    # Get the camera index from the request parameters (default to 0 if not provided)
+    camera_index = int(request.GET.get("camera_index", 0))
+
+    # Create an instance of VideoCamera with the specified camera index
+    video_camera = VideoCamera(camera_index=camera_index)
+
     response = StreamingHttpResponse(
-        VideoCamera().generate_frames(),
+        video_camera.generate_frames(),
         content_type="multipart/x-mixed-replace;boundary=frame",
     )
 
