35bd10d18a648be505471b95dd18702c208d1938
Jayant <jayantlaldas@gmail.com>
2023-11-10 11:56:13 +0530

    firebase

--- faceRecog/api/camera.py
+++ faceRecog/api/camera.py
@@ -25,9 +25,9 @@ class VideoCamera(object):
                 "universe_domain": "googleapis.com",
             }
         )
-        # firebase_admin.initialize_app(cred)
+        firebase_admin.initialize_app(cred)
 
-        # self.db = firestore.client()
+        self.db = firestore.client()
 
         # Initialize SimpleFacerec or your face recognition logic here...
         self.sfr = SimpleFacerec()
@@ -51,19 +51,8 @@ class VideoCamera(object):
             for face_loc, name in zip(face_locations, face_names):
                 y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
-                if name != "Unknown":
-                    cv2.putText(
-                        frame,
-                        name,
-                        (x1, y1 - 10),
-                        cv2.FONT_HERSHEY_DUPLEX,
-                        1,
-                        (0, 0, 200),
-                        2,
-                    )
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
-
-                else:
+                if name == "Unknown":
+                    # print(name + "if")
                     cv2.putText(
                         frame,
                         name,
@@ -74,6 +63,19 @@ class VideoCamera(object):
                         2,
                     )
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 200, 0), 4)
+
+                else:
+                    print(name + "else")
+                    cv2.putText(
+                        frame,
+                        name,
+                        (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_DUPLEX,
+                        1,
+                        (0, 0, 200),
+                        2,
+                    )
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
 
                     current_time = time.time()
                     if current_time - self.last_data_time >= 10:
@@ -88,10 +90,10 @@ class VideoCamera(object):
                         timestamp_id = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
                         # Add the document to the "suspects" collection using the timestamp as the ID
-                        # self.db.collection("Suspects").document(timestamp_id).set(
-                        #    document_data
-                        # )
-                        print(name)
+                        self.db.collection("Suspects").document(timestamp_id).set(
+                            document_data
+                        )
+                        print(name + " Updated in Database")
 
                         # Update the last_data_time
                         self.last_data_time = current_time
